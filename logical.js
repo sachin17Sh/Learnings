9dba4680d12a7e247c68fbedca29d41400eb2e05
sachin <sharmasachinwins@gmail.com>
2024-04-23 11:42:19 +0530

    changes

--- logical.js
+++ logical.js
@@ -1,81 +1,83 @@
-// //copy within same array 
-// const ARY = [1,2,3,4,5,6,7,8]
-// // let length= ARY.length
+//copy within same array 
+const ARY = [1,2,3,4,5,6,7,8]
+ let length= ARY.length
 
-// for (let i = 0; i < length; i++) {
-//      ARY[length+i]=ARY[i]
+for (let i = 0; i < length; i++) {
+     ARY[length+i]=ARY[i]
     
-// }
-// // console.log(ARY);
+}
+// console.log(ARY);
 
 
 
 
-// //Finding length without inbuilt functions 
-// const a=[1,2,3,4,5,6,7,8,9]
-// let length =0
-// while (a[length]!=undefined){
+//Finding length without inbuilt functions 
+const myary=[1,2,3,4,5,6,7,8,9]
+let len =0
+while (myary[len]!=undefined){
    
-//     length++
+    len++
     
-// }
-// console.log(length);
+}
+// console.log(len);
 
 
 
 
-//star pattern 
-// let n = 5
-// for (let i = 0; i <= n; i++) {
-//       str=""
-//     for (let j = 0; j <=i; j++) {
-//         str+= "* "
+// star pattern 
+let n = 5
+for (let i = 0; i <= n; i++) {
+      str=""
+    for (let j = 0; j <=i; j++) {
+        str+= "* "
        
-//     }
-//     console.log(str);
-// }
-// let n = 5;
+    }
+    // console.log(str);
+}
+let m = 5;
 
-// for (let i = n; i >= 0; i--) {
-//     let str = "";
+for (let k = m; k >= 0; k--) {
+    let star = "";
     
-//     for (let j = 0; j <= i; j++) {
-//         str += "* ";
-//     }
+    for (let l = 0; l <= k; l++) {
+        star += "* ";
+    }
     
-//     console.log(str);
-// }
+    // console.log(star);
 
+}
 
 
 
 
 
 // checking if the given number already exist in array
-// const a =[1,7,98,6,5]
-// b = 5
-//  let result = ""
-
-// for (let i = 0; i < a.length; i++) {
-//     // console.log(a[i]);
-//     if(b == a[i]){
-//         result = 'Value is exist'
-//         break;    
-//     }else {
-//         result ="Value not exist"
-//     } 
-// }
-// console.log(result);
-
-const a = [1, 7, 98, 6, 5];
-const b = 33;
-let result = "Value not exist"; 
+const a =[1,7,98,6,5]
+b = 5
+ let result = ""
 
 for (let i = 0; i < a.length; i++) {
-    if (b === a[i]) {
-        result = 'Value is exist';
-        break;
-    }
+    // console.log(a[i]);
+    if(b == a[i]){
+        result = 'Value  exist'
+        break;    
+    }else {
+        result ="Value not exist"
+    } 
 }
+// console.log(result);
 
-console.log(result);
+//another way
+
+// const a = [1, 7, 98, 6, 5];
+// const b = 33;
+// let result = "Value not exist"; 
+
+// for (let i = 0; i < a.length; i++) {
+//     if (b === a[i]) {
+//         result = 'Value  exist';
+//         break;
+//     }
+// }
+
+// console.log(result);
--- strings.js
+++
@@ -1,5 +0,0 @@
-let a = "Mynameissacgin"
-// console.log(a.length);
-let char = a.charAt(5)
-console.log(char);
-let n = name.at(2)
