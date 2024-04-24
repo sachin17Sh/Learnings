0dd7c5182c6f01bf4b6be29a101e97d6ee0a29c4
sachin <sharmasachinwins@gmail.com>
2024-04-24 09:23:09 +0530

    changes in star

--- javascript/starpattern.js
+++ javascript/starpattern.js
@@ -1,5 +1,6 @@
 
 // star pattern 
+//left  triangle 
 let n = 5
 for (let i = 0; i <= n; i++) {
     str = ""
@@ -9,9 +10,12 @@ for (let i = 0; i <= n; i++) {
     }
     // console.log(str);
 }
+
+
+//reverse left  triangles
 let m = 5;
 
-for (let k = m; k >= 0; k--) {
+for (let k = m; k >= m; k--) {
     let star = "";
 
     for (let l = 0; l <= k; l++) {
@@ -21,3 +25,19 @@ for (let k = m; k >= 0; k--) {
     // console.log(star);
 
 }
+
+//right triangles 
+let o = 10;
+for (let x = 1; x <= o; x++) {
+    let st = "";
+    for (let y = 0; y < o - x; y++) {
+        st += " ";
+    }
+    for (let z = 0; z < x; z++) {
+        st += "*";
+    }
+    console.log(st);
+}
+
+
+//right reverse triangles 
