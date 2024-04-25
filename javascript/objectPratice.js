90cfdba82155ed4543a336b939794b5fec651a3f
sachin <sharmasachinwins@gmail.com>
2024-04-25 09:43:10 +0530

    looping in object

--- javascript/objectPratice.js
+++ javascript/objectPratice.js
@@ -9,10 +9,24 @@ const data ={
     Nationality: "Indian"
 
 }
-console.log(Object.keys(data));
-console.log(Object.values(data));
-console.log(Object.entries(data));
+// console.log(Object.keys(data));
+// console.log(Object.values(data));
+// console.log(Object.entries(data));
 
 
 
 //Looping an array 
+//for printing keys only
+let key =[]
+for (const key in data) {
+    console.log(key);
+    }
+//for printing values
+let value = []
+for (const values in data) {
+    
+    console.log(data[values]);
+}
+
+
+console.log(data);
